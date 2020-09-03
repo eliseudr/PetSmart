d17350c3e06bc0fc2783cde74b84308d829228db
eliseudr <eliseudr@hotmail.com>
2020-09-02 23:32:13 -0300

    Removendo a class Slider, mudança no desing

--- pet_smart/lib/inicio_screen.dart
+++ pet_smart/lib/inicio_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pet_smart/widgets/slide_item.dart';
 
 class TelaInicial extends StatelessWidget {
   @override
@@ -13,7 +12,35 @@ class TelaInicial extends StatelessWidget {
           child: Column(
             children: <Widget>[
               Expanded(
-                child: SlideItem(),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      width: 200,
+                      height: 200,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          image: DecorationImage(image: AssetImage('assets/Imagens/logo.png'),
+                            fit: BoxFit.cover,
+                          )
+                      ),
+                    ),
+                    //Nome do app (PetSmart)
+                    SizedBox(height: 50),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      child: Text('PetSmart', style: TextStyle(fontSize: 24,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      ),
+                    ),
+                    SizedBox(height: 5,),
+                    Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor mi vitae arcu vestibulum, non gravida ante facilisis.',
+                      textAlign: TextAlign.left,
+                    ),
+                  ],
+                ),
               ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
