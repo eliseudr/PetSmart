caa5d8dab029d706091cc80c44c9e252e17dd597
eliseudr <eliseudr@hotmail.com>
2020-09-06 22:14:51 -0300

    Lista de servicoes adicionada

--- pet_smart/lib/home_screen.dart
+++ pet_smart/lib/home_screen.dart
@@ -8,6 +8,7 @@ class HomeTela extends StatefulWidget {
 
 class _HomeTelaState extends State<HomeTela> {
 
+  //Todo esse botao nao vai existir quando a tela for finalizada, sim apenas um botao SAIR
   _buildBtnVoltar(){
     return Container(
       width: 1000,
@@ -25,6 +26,82 @@ class _HomeTelaState extends State<HomeTela> {
     );
   }
 
+  _buildServicos(){
+    return Container(
+        padding: EdgeInsets.only(top: 12, left: 12),
+        alignment: Alignment.topLeft,
+        child: Column(
+          children: <Widget>[
+            Text('Solicitação de Serviços',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+            ),
+          ],
+        ),
+
+    );
+  }
+
+  _buildListServicos(){
+    return Padding(
+      padding: const EdgeInsets.only(left: 12),
+      child: Container(
+        height: 160,
+          child: ListView(
+            padding: EdgeInsets.only(right: 12,),
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(40),
+                    child: Image.asset('assets/Imagens/Consulta.png', height: 130,),
+                  ),
+                  SizedBox(height: 10,),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 35),
+                    child: Text('Consultar', style: TextStyle(fontWeight: FontWeight.w900, color: Colors.black54),),
+                  ),
+                ],
+              ),
+              //Segunda Imagem
+              SizedBox(width: 14,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(40),
+                    child: Image.asset('assets/Imagens/Banho.png', height: 130,),
+                  ),
+                  SizedBox(height: 10,),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 45),
+                    child: Text('Banho', style: TextStyle(fontWeight: FontWeight.w900, color: Colors.black54),),
+                  ),
+                ],
+              ),
+              //Terceira imagem
+              SizedBox(width: 14,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(40),
+                    child: Image.asset('assets/Imagens/Tosa.png', height: 130,),
+                  ),
+                  SizedBox(height: 10,),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 45),
+                    child: Text('Banho', style: TextStyle(fontWeight: FontWeight.w900, color: Colors.black54),),
+                  ),
+                ],
+              ),
+            ],
+          ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,12 +114,17 @@ class _HomeTelaState extends State<HomeTela> {
               //botao voltar IOs
               SizedBox(height: 8,),
               _buildBtnVoltar(),
+              SizedBox(height: 40,),
+              _buildServicos(),
+              SizedBox(height: 12,),
+              _buildListServicos(),
               Expanded(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    //todo campos
+                    // _buildServicos(),
+                    //todo campos meus pets
                   ],
                 ),
               )
